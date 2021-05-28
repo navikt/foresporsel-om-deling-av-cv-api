291c52260d26930bcc93e40edfb3cef3eadc280d
frodank <11638682+frodank@users.noreply.github.com>
2021-05-28 12:40:24 +0200

    Logge sending av kafka-meldinger

--- src/main/kotlin/KafkaService.kt
+++ src/main/kotlin/KafkaService.kt
@@ -2,6 +2,7 @@ import no.nav.rekrutteringsbistand.avro.ForesporselOmDelingAvCvKafkamelding
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
 import stilling.Stilling
+import utils.log
 import java.util.*
 
 // TODO
@@ -14,9 +15,12 @@ class KafkaService(
 ) {
 
     fun sendUsendteForespørsler() {
-        repository.hentUsendteForespørsler().forEach {
+        val usendteForespørsler = repository.hentUsendteForespørsler()
+        log.info("Fant ${usendteForespørsler.size} usendte forespørsler")
+        usendteForespørsler.forEach {
 
             val stilling = hentStilling(it.stillingsId)
+            log.info("Hentet stilling $stilling")
 
             val melding = ProducerRecord(topic, it.aktørId, it.tilKafkamelding(stilling))
             producer.send(melding)
