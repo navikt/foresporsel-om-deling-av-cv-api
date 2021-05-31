5925929505365134af146a273abf41c93970b66b
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-05-31 10:41:51 +0200

    Legg til TODO om å skru på Kafka-melding igjen

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -65,6 +65,8 @@ fun main() {
         val database = Database()
         val repository = Repository(database.dataSource)
         val controller = Controller(repository)
+        
+        // TODO: Skru på KafkaProducer for prod
         val producer = when (Cluster.current) {
             Cluster.DEV_FSS -> KafkaProducer<String, ForesporselOmDelingAvCvKafkamelding>(producerConfig)
             Cluster.PROD_FSS -> MockProducer(true, null, null)
