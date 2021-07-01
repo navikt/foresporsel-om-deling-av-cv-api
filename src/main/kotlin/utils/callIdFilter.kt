211d7f1724addd5f0992dce6e23652de45000a80
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-07-01 14:49:00 +0200

    Loggfør call ID

--- src/main/kotlin/utils/callIdFilter.kt
+++ src/main/kotlin/utils/callIdFilter.kt
@@ -6,8 +6,19 @@ import java.util.*
 const val foretrukkenCallIdHeaderKey = "Nav-Call-Id"
 
 val settCallId: (Context) -> Unit = {
-    val callId = it.header(foretrukkenCallIdHeaderKey) ?: it.header("Nav-CallId") ?: it.header("X-Nav-CallId") ?: UUID.randomUUID().toString()
+    val foretrukkenCallId = it.header(foretrukkenCallIdHeaderKey)
+    val alternativCallId = it.header("Nav-CallId")
+    val sisteCallId = it.header("X-Nav-CallId")
+
+    log("settCallId").info("foretrukkenCallId: $foretrukkenCallId, alternativ: $alternativCallId, siste: $sisteCallId")
+
+    val callId = foretrukkenCallId ?: alternativCallId ?: sisteCallId ?: UUID.randomUUID().toString()
     it.attribute(foretrukkenCallIdHeaderKey, callId)
 }
 
-fun Context.hentCallId() = UUID.fromString(attribute(foretrukkenCallIdHeaderKey)!!)
+fun Context.hentCallId(): UUID {
+    val callId: String? = attribute(foretrukkenCallIdHeaderKey)
+    log("hentCallId").info("Hentet callId: $callId")
+
+    return UUID.fromString(callId!!)
+}
