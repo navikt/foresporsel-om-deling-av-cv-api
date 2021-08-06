1ecbe4c12ab7b75540f2162ba623769f3410f08d
André Mossige <andre.mossige@nav.no>
2021-08-06 13:07:14 +0200

    Bruk riktig topic for å motta svar på forespørsel

--- src/main/kotlin/mottasvar/SvarService.kt
+++ src/main/kotlin/mottasvar/SvarService.kt
@@ -10,7 +10,7 @@ import java.io.Closeable
 import java.time.Duration
 import java.util.*
 
-val svarTopic = TopicPartition("arbeidsgiver-inkludering.svar-pa-deling-av-cv", 0) // TODO: Bruk topic PTO har opprettet
+val svarTopic = TopicPartition("pto.stilling-fra-nav-oppdatert-v1", 0)
 
 class SvarService(
     private val consumer: Consumer<String, SvarPaForesporselOmDelingAvCv>,
