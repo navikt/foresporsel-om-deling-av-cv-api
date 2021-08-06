87d93fe94448a5cdfdd5793b02ce07a5f3a82d33
André Mossige <andre.mossige@nav.no>
2021-08-06 12:54:42 +0200

    Send forespørsel på riktig topic

--- src/main/kotlin/sendforespørsel/ForespørselService.kt
+++ src/main/kotlin/sendforespørsel/ForespørselService.kt
@@ -9,7 +9,7 @@ import stilling.Stilling
 import utils.log
 import java.util.*
 
-const val forespørselTopic = "arbeidsgiver-inkludering.foresporsel-om-deling-av-cv"
+const val forespørselTopic = "pto.deling-av-stilling-fra-nav-forespurt-v1"
 
 class ForespørselService(
     private val producer: Producer<String, ForesporselOmDelingAvCv>,
