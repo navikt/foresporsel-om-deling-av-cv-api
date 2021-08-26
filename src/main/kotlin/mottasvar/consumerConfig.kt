44778d6b5a58e37e79f7316230602563b5d63f2e
André Mossige <andre.mossige@nav.no>
2021-08-26 10:10:03 +0200

    Tving bruk av spesifikt Avro-skjema istedenfor "GenericDataRecord"

--- src/main/kotlin/mottasvar/consumerConfig.kt
+++ src/main/kotlin/mottasvar/consumerConfig.kt
@@ -16,6 +16,7 @@ val consumerConfig = mapOf(
     ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java.name,
     ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java.name,
 
+    KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG to "true",
     KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG to System.getenv("KAFKA_SCHEMA_REGISTRY"),
     KafkaAvroDeserializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE to "USER_INFO",
     KafkaAvroDeserializerConfig.USER_INFO_CONFIG to "${System.getenv("KAFKA_SCHEMA_REGISTRY_USER")}:${System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")}",
