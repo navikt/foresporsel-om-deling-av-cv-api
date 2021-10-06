de560c195f3561737b95615ee8dbce32c599802d
Are Husby <are.husby@nav.no>
2021-10-06 11:07:23 +0200

    Oppdater til nyeste Kotlin-version for Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.0"
+    kotlin("jvm") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "7.1.0"
     id("com.github.davidmc24.gradle.plugin.avro") version "1.2.0"
     application
