6f7266242fa90bfc10429e26308c4cf22a0a4d46
Mads Lee Giil <74543378+madsleegiil@users.noreply.github.com>
2022-03-21 10:38:47 +0100

    Godta kun Azure-tokens med denne appen som scope (#15)
    * Godta kun Azure-tokens med denne appen som scope
    
    * Bruk bearer token i tester framfor Cookie
    
    * Renaming fra isso til azure

--- src/test/kotlin/TokenUtilsTest.kt
+++ src/test/kotlin/TokenUtilsTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import setup.medVeilederCookie
+import setup.medVeilederToken
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TokenUtilsTest {
@@ -34,7 +34,7 @@ class TokenUtilsTest {
     @Test
     fun `Sikrede endepunkter skal returnere 401 hvis requesten inneholder et ugyldig token`() {
         val (_, response, _) = Fuel.post("http://localhost:8333/foresporsler")
-            .header("Cookie", "isso-idtoken=${hentUgyldigToken().serialize()}")
+            .header("Authorization", "Bearer ${hentUgyldigToken().serialize()}")
             .response()
 
         assertThat(response.statusCode).isEqualTo(401)
@@ -43,7 +43,7 @@ class TokenUtilsTest {
     @Test
     fun `Sikrede endepunkter skal returnere noe annet enn 401 hvis requesten inneholder et gyldig token`() {
         val (_, response, _) = Fuel.post("http://localhost:8333/foresporsler")
-            .medVeilederCookie(mockOAuth2Server)
+            .medVeilederToken(mockOAuth2Server)
             .response()
 
         assertThat(response.statusCode).isNotEqualTo(401)
--- src/test/kotlin/setup/tokenUtils.kt
+++ src/test/kotlin/setup/tokenUtils.kt
@@ -4,8 +4,8 @@ import com.github.kittinunf.fuel.core.Request
 import navIdentClaimKey
 import no.nav.security.mock.oauth2.MockOAuth2Server
 
-fun Request.medVeilederCookie(mockOAuth2Server: MockOAuth2Server, navIdent: String = "X12345"): Request {
-    return this.header("Cookie", "isso-idtoken=${hentToken(navIdent, mockOAuth2Server)}")
+fun Request.medVeilederToken(mockOAuth2Server: MockOAuth2Server, navIdent: String = "X12345"): Request {
+    return this.header("Authorization", "Bearer ${hentToken(navIdent, mockOAuth2Server)}")
 }
 
 fun hentToken(navIdent: String, mockOAuth2Server: MockOAuth2Server): String {
