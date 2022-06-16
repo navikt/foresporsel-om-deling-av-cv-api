41bde1b5b6173d5e1d34a86fc88a272c0833197e
Joar Aurdal <joar.aurdal@nav.no>
2022-06-16 15:28:14 +0200

    Oppgrader til kotlin 1.7

--- src/main/kotlin/stilling/StillingKlient.kt
+++ src/main/kotlin/stilling/StillingKlient.kt
@@ -30,81 +30,83 @@ class StillingKlient(private val accessToken: () -> String) {
             }
         }
     }
-}
 
-private data class EsResponse(
-    val _source: EsSource
-) {
-    fun toStilling(): Stilling = Stilling(
-        stillingtittel = _source.stilling.title,
-        søknadsfrist = _source.stilling.properties.applicationdue,
-        arbeidsgiver = _source.stilling.employer.name,
-        arbeidssteder = _source.stilling.locations.map(EsArbeidssted::toArbeidssted),
-        kontaktinfo = _source.stilling.contacts.map(EsContact::toKontakt),
-        stillingskategori = _source.stillingsinfo?.stillingskategori
-    )
-
-    private data class EsSource(
-        val stilling: EsStilling,
-        val stillingsinfo: EsStillingsinfo?
-    )
-
-    private data class EsStilling(
-        val title: String,
-        val properties: Properties,
-        val employer: Employer,
-        val locations: List<EsArbeidssted>,
-        val contacts: List<EsContact>
-    )
-
-    private data class EsStillingsinfo(
-        val eierNavident: String?,
-        val eierNavn: String?,
-        val notat: String?,
-        val stillingsid: String,
-        val stillingsinfoid: String,
-        val stillingskategori: String?,
-    )
-
-    private data class EsContact(
-        val name: String,
-        val title: String,
-        val email: String,
-        val phone: String,
-        val role: String
-    ){
-        fun toKontakt() = Kontakt(
-            navn = name,
-            tittel = title,
-            epost = email,
-            mobil = phone,
-            rolle = role
-        )
-    }
-
-    private data class Employer(
-        val name: String
-    )
-
-    private data class Properties(
-        val applicationdue: String?
-    )
-
-    private data class EsArbeidssted(
-        val address: String?,
-        val postalCode: String?,
-        val city: String?,
-        val county: String?,
-        val municipal: String?,
-        val country: String,
+    private data class EsResponse(
+        val _source: EsSource
     ) {
-        fun toArbeidssted() = Arbeidssted(
-            adresse = address,
-            postkode = postalCode,
-            by = city,
-            kommune = municipal,
-            fylke = county,
-            land = country
+        fun toStilling(): Stilling = Stilling(
+            stillingtittel = _source.stilling.title,
+            søknadsfrist = _source.stilling.properties.applicationdue,
+            arbeidsgiver = _source.stilling.employer.name,
+            arbeidssteder = _source.stilling.locations.map(EsArbeidssted::toArbeidssted),
+            kontaktinfo = _source.stilling.contacts.map(EsContact::toKontakt),
+            stillingskategori = _source.stillingsinfo?.stillingskategori
         )
+
+        private data class EsSource(
+            val stilling: EsStilling,
+            val stillingsinfo: EsStillingsinfo?
+        )
+
+        private data class EsStilling(
+            val title: String,
+            val properties: Properties,
+            val employer: Employer,
+            val locations: List<EsArbeidssted>,
+            val contacts: List<EsContact>
+        )
+
+        private data class EsStillingsinfo(
+            val eierNavident: String?,
+            val eierNavn: String?,
+            val notat: String?,
+            val stillingsid: String,
+            val stillingsinfoid: String,
+            val stillingskategori: String?,
+        )
+
+        private data class EsContact(
+            val name: String,
+            val title: String,
+            val email: String,
+            val phone: String,
+            val role: String
+        ){
+            fun toKontakt() = Kontakt(
+                navn = name,
+                tittel = title,
+                epost = email,
+                mobil = phone,
+                rolle = role
+            )
+        }
+
+        private data class Employer(
+            val name: String
+        )
+
+        private data class Properties(
+            val applicationdue: String?
+        )
+
+        private data class EsArbeidssted(
+            val address: String?,
+            val postalCode: String?,
+            val city: String?,
+            val county: String?,
+            val municipal: String?,
+            val country: String,
+        ) {
+            fun toArbeidssted() = Arbeidssted(
+                adresse = address,
+                postkode = postalCode,
+                by = city,
+                kommune = municipal,
+                fylke = county,
+                land = country
+            )
+        }
     }
 }
+
+
