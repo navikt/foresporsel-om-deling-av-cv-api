e3d551a0c703d9ff9b5cd80bad3b1aff53b250dd
Joar Aurdal <joar.aurdal@nav.no>
2022-07-27 11:09:13 +0200

    Legg til dummytest for rapid

--- src/main/kotlin/kandidatevent/KandidatLytter.kt
+++ src/main/kotlin/kandidatevent/KandidatLytter.kt
@@ -7,7 +7,15 @@ import no.nav.helse.rapids_rivers.River
 import utils.log
 
 class KandidatLytter(private val rapidsConnection: RapidsConnection) : River.PacketListener {
+    init {
+        River(rapidsConnection).apply {
+            validate {
+                it.demandKey("kandidat.dummy.cv-delt-med-arbeidsgiver-via-rekrutteringsbistand")
+            }
+        }.register(this)
+    }
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
+
         log.info("Mottok kandidatevent: $JsonMessage")
     }
 
