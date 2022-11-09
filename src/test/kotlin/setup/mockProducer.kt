da5d22747a9ba640610178c1ffa51bd18811d045
vildestefferud <qwartz.ms@hotmail.com>
2022-11-09 12:27:42 +0100

    Oppdatere README

--- src/test/kotlin/setup/mockProducer.kt
+++ src/test/kotlin/setup/mockProducer.kt
@@ -5,11 +5,12 @@ import org.apache.kafka.clients.producer.MockProducer
 import org.apache.kafka.common.Cluster
 import org.apache.kafka.common.serialization.StringSerializer
 
-val dummyForesporselOmDelingAvSvSerializer = { _: String, _: ForesporselOmDelingAvCv -> ByteArray(0) }
 
-val mockProducerAvro = MockProducer(true, StringSerializer(), dummyForesporselOmDelingAvSvSerializer)
+val dummyForesporselOmDelingAvCvSerializer = { _: String, _: ForesporselOmDelingAvCv -> ByteArray(0) }
+
+val mockProducerAvro = MockProducer(true, StringSerializer(), dummyForesporselOmDelingAvCvSerializer)
 
 val mockProducerJson = MockProducer(Cluster.empty(), false, null, StringSerializer(), StringSerializer())
 
 fun mockProducerUtenAutocomplete(): MockProducer<String, ForesporselOmDelingAvCv> =
-    MockProducer(false, StringSerializer(), dummyForesporselOmDelingAvSvSerializer)
+    MockProducer(false, StringSerializer(), dummyForesporselOmDelingAvCvSerializer)
